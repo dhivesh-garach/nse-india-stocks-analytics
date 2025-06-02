42e1cdb2dd27fb412f701651dbbc1a3ee5ce8bd3
Dhivesh G <div.xcel@gmail.com>
2025-06-02 14:11:45 +0000

    ingested historical stock data

--- extract_nse_stocks_data.py
+++ extract_nse_stocks_data.py
@@ -14,20 +14,26 @@ nifty_15_stocks = [
     "LICI.BSE", "LT.BSE", "KOTAKBANK.BSE", "SUNPHARMA.BSE", "HCLTECH.BSE"
 ]
 
-params = {
-    'function': 'TIME_SERIES_DAILY',
-    'symbol': 'HDFCBANK.BSE',
-    'outputsize': 'full',
-    'apikey': API_KEY
-}
+for element in nifty_15_stocks:
+    symbol = element
+    print(f"Fetching data for {symbol}...")
 
-response = requests.get(BASE_URL, params=params)
+    params = {
+        'function': 'TIME_SERIES_DAILY',
+        'symbol': symbol,
+        'outputsize': 'full',
+        'apikey': API_KEY
+    }
 
-if response.status_code == 200:
-    data = response.json()
+    response = requests.get(BASE_URL, params=params)
 
-    print(data) # Check if the response contains the expected data
+    if response.status_code == 200:
+        data = response.json()
+        pd.json_normalize(data['Time Series (Daily)']).to_csv(f"{symbol}_daily_data.csv")
 
+        print(f"Data for {symbol} saved successfully.")
+    else:
+        print(f"Error fetching data for {symbol}: {response.status_code}")
 
     
 
